0bac8f7badc270e7a023938d375aa96cccd2a09c
thairesv <145288735+thairesv@users.noreply.github.com>
2023-11-21 17:22:10 +0000

    Created App.js for part 3 of Homework2

---
+++ about-us/src/App.js
@@ -0,0 +1,46 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Group 1</h1>
+        <h2>Team Introduction</h2>
+        <div class="team-members">
+          <h3>Hi, My Name is Thaires</h3>
+          <p>✏️ One of my favourite hobbies is drawing 🖊️</p>
+          <p>
+            I love drawing nature and animals, I find it realxing and somtimes I
+            gift it to friends and family or decorate my own house
+          </p>
+        </div>
+        <div class="team-members">
+          <h3>Team Member</h3>
+          <p>Favourite hobby or activity you like to do (e.g., for downtime)</p>
+          <p>
+            Favourite hobby or activity you like to do (e.g., for downtime) Why
+            do you do it? Why do you enjoy it?
+          </p>
+        </div>
+        <div class="team-members">
+          <h3>Team Member</h3>
+          <p>Favourite hobby or activity you like to do (e.g., for downtime)</p>
+          <p>
+            Favourite hobby or activity you like to do (e.g., for downtime) Why
+            do you do it? Why do you enjoy it?
+          </p>
+        </div>
+        <div class="team-members">
+          <h3>Team Member</h3>
+          <p>Favourite hobby or activity you like to do (e.g., for downtime)</p>
+          <p>
+            Favourite hobby or activity you like to do (e.g., for downtime) Why
+            do you do it? Why do you enjoy it?
+          </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
