9a2ec092139628fe273c8a564a182934754576e8
thatsherbusiness <126254062+thatsherbusiness@users.noreply.github.com>
2023-12-03 18:04:01 +0000

    Adding logo

--- RealorNotReal/src/components/pages/IntroScreen.js
+++ RealorNotReal/src/components/pages/IntroScreen.js
@@ -7,7 +7,7 @@ function IntroScreen() {
   //Navigation screen for user to choose to play quiz or game
   return (
     <div className="intro-menu">
-      
+      <img className="logo" src="RealorNotReal/src/components/images/real-or-no-real-logo_white.svg" alt="Real or No Real"></img>
       <h1>Information</h1>
       <button className="accent-button" onClick={() => navigate("/quiz")}>Take the knowledge quiz</button>
       <button onClick={() => navigate("/game")}>Play Real or No Real</button>
