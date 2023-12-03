796f1d3a6bfb7dec35e542a8f7d354ff31f599f2
thatsherbusiness <126254062+thatsherbusiness@users.noreply.github.com>
2023-12-03 19:55:54 +0000

    Trying to fix logo not showing

--- RealorNotReal/src/components/pages/IntroScreen.js
+++ RealorNotReal/src/components/pages/IntroScreen.js
@@ -7,10 +7,12 @@ function IntroScreen() {
   //Navigation screen for user to choose to play quiz or game
   return (
     <div className="intro-menu">
-      <img className="logo" src="RealorNotReal/src/components/images/real-or-no-real-logo_white.svg" alt="Real or No Real"></img>
+      <img className="logo" src="./RealorNotReal/src/components/images/real-or-no-real-logo_white.png" alt="Real or No Real"></img>
       <h1 className="tagline">Can <i>you</i> spot a deep fake?</h1>
-      <button className="accent-button equal-button" onClick={() => navigate("/quiz")}>Take the knowledge quiz</button>
-      <button className="equal-button" onClick={() => navigate("/game")}>Play Real or No Real</button>
+      <container id="buttons">
+        <button className="accent-button equal-button" onClick={() => navigate("/quiz")}>Take the knowledge quiz</button>
+        <button className="equal-button" onClick={() => navigate("/game")}>Play Real or No Real</button>
+      </container>
     </div>
   );
 }
