57b890abfac4e930693ee655eb6817afe2055a99
thairesv <145288735+thairesv@users.noreply.github.com>
2023-12-05 14:53:12 +0000

    Fixed merging error

--- RealorNotReal/src/components/pages/IntroScreen.js
+++ RealorNotReal/src/components/pages/IntroScreen.js
@@ -7,11 +7,8 @@ function IntroScreen() {
   //Navigation screen for user to choose to play quiz or game
   return (
     <div className="intro-menu">
-<<<<<<< HEAD
+
       <img className="logo" src="/real-or-no-real-logo_white.svg" alt="Real or No Real" />
-=======
-      <img className="logo" src="./RealorNotReal/src/components/images/real-or-no-real-logo_white.png" alt="Real or No Real"></img>
->>>>>>> a2a0e0616817eb7c6b0f2ae70c16cef3f24666bc
       <h1 className="tagline">Can <i>you</i> spot a deep fake?</h1>
       <container className="row">
         <container className="column">
